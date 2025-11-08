3ded3e0b8ecf12b9dd293ca38155ea179519dd3a
Prabh <prabh2004p@gmail.com>
2025-11-08 14:57:40 -0500

    final commit

--- app.py
+++ app.py
@@ -6,7 +6,19 @@ from nltk.corpus import stopwords
 nltk.download("stopwords")
 nltk.download("punkt_tab")
 import pickle
+import requests
+import os
 
+
+
+
+def fetch_poster(movie_id):
+       API_KEY = "8cc67305b76b904f9ee991a5e895e86f"
+       url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}&language=en-US"
+       response = requests.get(url)
+       data = response.json()
+       poster_path = data.get("poster_path")
+       return "https://www.themoviedb.org/t/p/w1280" + poster_path
 
 st.header("Movie Recommender System")
 df= pickle.load(open("Artifacts/Models/data.pkl","rb"))
@@ -16,17 +28,27 @@ model = pickle.load(open("Artifacts/Models/sim.pkl","rb"))
 
 movies = st.selectbox('Select the Movie',movies_list.values)
 
-if st.button("Recommend Similar Movies"):
-    def recommend(movie_title):
-    # find index of the movie
-     movie_index = df[df['title'].str.lower() == movie_title.lower()].index[0]
-    
-     distances = model[movie_index]
-    
-     similar_movies = sorted(list(enumerate(distances)), key=lambda x: x[1], reverse=True)
-    
-     for i in similar_movies[1:6]:  # skip the first one (itself)
-        st.write(df.iloc[i[0]].title)
+def recommend(movie_title):
+    recommend_movie = []
+    recommend_poster = []
 
-    recommend(movies)
+    movie_index = df[df['title'].str.lower() == movie_title.lower()].index[0]
+    distances = model[movie_index]
+    similar_movies = sorted(list(enumerate(distances)), key=lambda x: x[1], reverse=True)[1:6]
+
+    for i in similar_movies:
+        movie_id = df.iloc[i[0]].movie_id  # ✅ fetch ID for each similar movie
+        recommend_movie.append(df.iloc[i[0]].title)
+        recommend_poster.append(fetch_poster(movie_id))
+
+    return recommend_movie, recommend_poster
+
+
+if st.button("Recommend Movie"):
+     movie,poster = recommend(movies)
+     cols= st.columns(5)
+     for idx,col in enumerate(cols):
+             st.subheader(movie[idx])
+             st.image(poster[idx],width=100)
+
      
